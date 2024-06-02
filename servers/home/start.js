bf7e6d97bb532b5ba916fe452388b3ce9f31f8e2
doubleface <christophe@cozycloud.cc>
2024-06-02 05:20:54 +0200

    feat: update and deploy n00dles on start

--- servers/home/start.js
+++ servers/home/start.js
@@ -1,7 +1,10 @@
+import { runAndWait } from 'libs/utils.js'
 /** @param {NS} ns */
 export async function main(ns) {
   for (const file of ns.ls('home', 'data/actions/')) {
     ns.rm(file, 'home')
   }
   ns.run('demon.js')
+  await runAndWait(ns, 'update.js')
+  await runAndWait(ns, 'deploy.js', 1, 'n00dles', '-g', 7, '-h', 4, '-w', 1, '--safetyFactor', 1)
 }
